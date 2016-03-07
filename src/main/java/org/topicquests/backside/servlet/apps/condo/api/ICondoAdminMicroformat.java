02be0d2615194399de18ffaaa5e0349e02af2779
Jack Park <jackpark@gmail.com>
2016-03-07 13:53:24 -0800

    first git

---
+++ src/main/java/org/topicquests/backside/servlet/apps/condo/api/ICondoAdminMicroformat.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package org.topicquests.backside.servlet.apps.condo.api;
+
+/**
+ * @author park
+ *
+ */
+public interface ICondoAdminMicroformat {
+
+}
---
+++ src/main/java/org/topicquests/backside/servlet/apps/condo/api/ICondoAdminModel.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package org.topicquests.backside.servlet.apps.condo.api;
+
+/**
+ * @author Admin
+ *
+ */
+public interface ICondoAdminModel {
+
+}

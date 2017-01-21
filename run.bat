4e57453cbfd53176252e42cf97ca01f3fb313de4
Alec Wenzowski <alec@wenzowski.com>
2017-01-21 04:28:28 -0500

    remove wildcard from run script

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -Xms1024M -Xmx4G  -jar .\target\backside-servlet-ks-*.jar
+java -Xms1024M -Xmx4G  -jar .\target\backside-servlet-ks-0.9.0-SNAPSHOT.jar
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xms1024M -Xmx4G -jar target/backside-servlet-ks-*.jar
+java -Xms1024M -Xmx4G -jar target/backside-servlet-ks-0.9.0-SNAPSHOT.jar
